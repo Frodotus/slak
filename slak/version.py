0ececd1e50c3352e98daa00a3adf91a5c4601bca
Toni Leino <toni@leino.net>
2026-06-17 12:16:13 +0300

    Release 0.1.4

--- slak/version.py
+++ slak/version.py
@@ -23,7 +23,7 @@ carries the unofficial / TOS warning the overview requires.
 
 from __future__ import annotations
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 AUTHOR = "Toni Leino"
 URL = "https://github.com/Frodotus/slak"
