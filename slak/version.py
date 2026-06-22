6ca15bbb1bb952c2dcb59298727420d152ba6a8c
Toni Leino <toni@leino.net>
2026-06-22 20:20:56 +0300

    Release 26.6.7

--- slak/version.py
+++ slak/version.py
@@ -23,7 +23,7 @@ carries the unofficial / TOS warning the overview requires.
 
 from __future__ import annotations
 
-__version__ = "26.6.6"
+__version__ = "26.6.7"
 
 AUTHOR = "Toni Leino"
 URL = "https://github.com/Frodotus/slak"
